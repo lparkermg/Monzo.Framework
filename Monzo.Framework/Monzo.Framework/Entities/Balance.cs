efe8fccf91a9a034abbe34f6707c676c83d30c9e
Kiran94 <kiran_patel94@hotmail.com>
2018-02-03 20:54:59 +0000

    #7 Added balance entity

---
+++ Monzo.Framework/Monzo.Framework/Entities/Balance.cs
@@ -0,0 +1,31 @@
+﻿namespace Monzo.Framework.Entities
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Represents a Balance.
+    /// </summary>
+    public class Balance
+    {
+        /// <summary>
+        /// Gets or sets the balance amount.
+        /// </summary>
+        /// <value>The balance amount.</value>
+        [JsonProperty("balance")]
+        public long BalanceAmount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the currency.
+        /// </summary>
+        /// <value>The currency.</value>
+        public string Currency { get; set; }
+
+        /// <summary>
+        /// Gets or sets the spent today amount.
+        /// </summary>
+        /// <value>The spent today.</value>
+        [JsonProperty("spend_today")]
+        public long SpentToday { get; set; }
+    }
+}
