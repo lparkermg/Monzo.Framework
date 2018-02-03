4804a66dd1fe846bc7b50fc6db5fff38ac04ab27
Kiran94 <kiran_patel94@hotmail.com>
2018-02-03 12:12:20 +0000

    Fixes #5 - Implemented the JSON service

---
+++ Monzo.Framework/Monzo.Framework/Services/JSONService.cs
@@ -0,0 +1,36 @@
+﻿namespace Monzo.Framework.Services
+{
+    using System;
+    using Monzo.Framework.Contracts;
+    using Newtonsoft.Json;
+
+    /// <inheritdoc />
+    public class JSONService : IJSONService
+    {
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Monzo.Framework.Services.JSONService"/> class.
+        /// </summary>
+        /// <param name="logger">Logger.</param>
+        public JSONService(ILogger logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        /// <inheritdoc />
+        public T Parse<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);           
+        }
+
+        /// <inheritdoc />
+        public string Parse<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
