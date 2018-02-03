187d4041d3321ff15ad73555239506bed1f2a672
Kiran94 <kiran_patel94@hotmail.com>
2018-02-03 19:55:57 +0000

    #6 - docs on accounts obj

--- Monzo.Framework/Monzo.Framework/Entities/Accounts.cs
+++ Monzo.Framework/Monzo.Framework/Entities/Accounts.cs
@@ -3,8 +3,15 @@
     using System.Collections.Generic;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Represents a list of accounts.
+    /// </summary>
     public class Accounts
     {
+        /// <summary>
+        /// Gets or sets the account collection.
+        /// </summary>
+        /// <value>The account collection.</value>
         [JsonProperty("accounts")]
         public List<Account> AccountCollection { get; set; }
     }
