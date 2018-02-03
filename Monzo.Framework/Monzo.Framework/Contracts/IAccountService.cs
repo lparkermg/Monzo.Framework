68f4252296fafbd80b684a2bf35f047cbaa4797c
Kiran94 <kiran_patel94@hotmail.com>
2018-02-03 12:44:10 +0000

    #6 added skeleton for account service

---
+++ Monzo.Framework/Monzo.Framework/Contracts/IAccountService.cs
@@ -0,0 +1,25 @@
+﻿namespace Monzo.Framework.Contracts
+{
+    using System.Collections.Generic;
+    using Monzo.Framework.Entities;
+    using Monzo.Framework.Enums;
+
+    /// <summary>
+    /// Encapsulates logic to interact with Account objects.
+    /// </summary>
+    public interface IAccountService
+    {
+        /// <summary>
+        /// Gets all Accounts.
+        /// </summary>
+        /// <returns>The accounts.</returns>
+        List<Account> GetAccounts();
+
+        /// <summary>
+        /// Gets the accounts fitered by the passed account type.
+        /// </summary>
+        /// <returns>The accounts.</returns>
+        /// <param name="type">account type to filter on.</param>
+        List<Account> GetAccounts(AccountType type);
+    }
+}
---
+++ Monzo.Framework/Monzo.Framework/Services/AccountService.cs
@@ -0,0 +1,36 @@
+﻿namespace Monzo.Framework.Services
+{
+
+    using System;
+    using System.Collections.Generic;
+    using Monzo.Framework.Contracts;
+    using Monzo.Framework.Entities;
+    using Monzo.Framework.Enums;
+
+    /// <inhertidoc />
+    public class AccountService : IAccountService
+    {
+        private readonly IHttpService httpService;
+        private readonly ILogger logger;
+
+        public AccountService(IHttpService httpService, ILogger logger, Authentication authentication)
+        {
+            this.httpService = httpService ?? throw new ArgumentNullException(nameof(httpService)); 
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger)); 
+        }
+
+        /// <inhertidoc />
+        public List<Account> GetAccounts()
+        {
+            // url builder?
+
+            throw new NotImplementedException();
+        }
+
+        /// <inhertidoc />
+        public List<Account> GetAccounts(AccountType type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
