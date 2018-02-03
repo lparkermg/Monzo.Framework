ca17726a4095bb95e8bb26ec25683400e5aff95a
Kiran94 <kiran_patel94@hotmail.com>
2018-02-03 12:43:52 +0000

    #6 Added Account entity

---
+++ Monzo.Framework/Monzo.Framework/Entities/Account.cs
@@ -0,0 +1,28 @@
+﻿namespace Monzo.Framework.Entities
+{
+    using System;
+
+    /// <summary>
+    /// Represents an Account.
+    /// </summary>
+    public class Account
+    {       
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created date.
+        /// </summary>
+        /// <value>The created.</value>
+        public DateTime Created { get; set; }
+    }
+}
