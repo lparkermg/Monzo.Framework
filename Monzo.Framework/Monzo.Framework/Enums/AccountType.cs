19139dcc95ceec57fe3a04781e357586245cf3c9
Kiran94 <kiran_patel94@hotmail.com>
2018-02-03 12:34:51 +0000

    Added Account type and Account type extensions to get the description

---
+++ Monzo.Framework/Monzo.Framework/Enums/AccountType.cs
@@ -0,0 +1,18 @@
+﻿namespace Monzo.Framework.Enums
+{
+    /// <summary>
+    /// Represents an Account Type.
+    /// </summary>
+    public enum AccountType
+    {
+        /// <summary>
+        /// The UK Retail (current account) type.
+        /// </summary>
+        UKRetail,
+
+        /// <summary>
+        /// The UK Prepaid (Beta) type.
+        /// </summary>
+        UKPrepaid                     
+    }
+}
