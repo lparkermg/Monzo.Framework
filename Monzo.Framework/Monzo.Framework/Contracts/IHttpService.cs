0b89f62c37bdacd7d6e10c0e58970487f4cec17e
Kiran94 <kiran_patel94@hotmail.com>
2018-02-03 11:23:10 +0000

    Fixes #3 Added HTTPService with GET method

---
+++ Monzo.Framework/Monzo.Framework/Contracts/IHttpService.cs
@@ -0,0 +1,20 @@
+﻿namespace Monzo.Framework.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Encapsulates logic to interact with an external HTTP service.
+    /// </summary>
+    public interface IHttpService
+    {
+        /// <summary>
+        /// GET the string response from the given uri and the given headers.
+        /// </summary>
+        /// <returns>The awaitable response.</returns>
+        /// <param name="uri">URI to GET.</param>
+        /// <param name="headers">Headers to add to the HTTP request.</param>
+        Task<string> Get(Uri uri, Dictionary<string, string> headers);
+    }
+}
---
+++ Monzo.Framework/Monzo.Framework/Services/HttpService.cs
@@ -0,0 +1,41 @@
+﻿namespace Monzo.Framework.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Monzo.Framework.Contracts;
+
+    /// <inheritdoc />
+    public sealed class HttpService : IHttpService
+    {
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger logger; 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Monzo.Framework.Services.HttpService"/> class.
+        /// </summary>
+        /// <param name="logger">injected logger.</param>
+        public HttpService(ILogger logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        /// <inheritdoc />
+        public async Task<string> Get(Uri uri, Dictionary<string, string> headers)
+        {
+            using(var client = new WebClient())
+            {
+                foreach(var currentHeader in headers)
+                {
+                    client.Headers.Set(currentHeader.Key, currentHeader.Value);
+                }
+                             
+                return await client.DownloadStringTaskAsync(uri.AbsolutePath);
+            }
+        }
+    }
+}
