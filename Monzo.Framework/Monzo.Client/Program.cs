c71fdf1a981a7e9223ab79ad62db838631148e4b
Kiran94 <kiran_patel94@hotmail.com>
2018-02-04 20:37:53 +0000

    Updated client

--- Monzo.Framework/Monzo.Client/Program.cs
+++ Monzo.Framework/Monzo.Client/Program.cs
@@ -10,14 +10,7 @@ namespace Monzo.Client
     public class MainClass
     {
         public static void Main(string[] args)
-        {
-            var log = LogManager.GetLogger(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
-            var configService = new ConfigurationService();
-            var logger = new Logger(log);
-            var jsonservice = new JSONService(logger);
-            var httpService = new HttpService(logger);
-            var authservice = new AuthenticationLocalService(configService, logger);
-
+        {           
             MonzoConfiguration configuration = new MonzoConfiguration();
 
             var accountService = new AccountService(configuration);
@@ -26,8 +19,22 @@ namespace Monzo.Client
             var account = acc.Result.AccountCollection.First();
             var transactionService = new TransactionService(configuration);
 
+            Console.WriteLine(account.ID);
+            Console.WriteLine(account.Description);
+            Console.WriteLine(account.Created);
+
+            var potservice = new PotService(configuration);
+            potservice.GetPotsAsync()
+
+            var transactions = transactionService.GetTransactionsAsync(account, false);
+           
             //var result = transactionService.GetTransactionsAsync(account, false).Result;
             var result2 = transactionService.GetTransactionsByDateAsync(account, new DateTime(2018, 01, 05), new DateTime(2018, 01, 10), false).Result;
+
+            var balanceservice = new BalanceService(configuration);
+            var balance = balanceservice.GetBalanceAsync(account).Result;
+
+            Console.WriteLine(balance.BalanceAmount);
 
             Console.WriteLine(result2);
 
