d5b23302f1398c824221083482e4b0b7a75de3a5
Luke Parker <moongateuk@gmail.com>
2018-03-25 19:59:14 +0100

    Added Depositing and Withdrawing from pots to IPotsService.

--- Monzo.Framework/Monzo.Framework/Contracts/IPotService.cs
+++ Monzo.Framework/Monzo.Framework/Contracts/IPotService.cs
@@ -14,5 +14,22 @@
         /// </summary>
         /// <returns>The pots.</returns>
         Task<Pots> GetPotsAsync();
+
+        /// <summary>
+        /// Deposits a stated amount into the specified pot.
+        /// </summary>
+        /// <param name="potId">Pot to deposit into.</param>
+        /// <param name="amount">Amount to deposit.</param>
+        /// <returns>If the deposit was successful or not.</returns>
+        Task<bool> DepositToPotAsync(string potId, long amount);
+
+        /// <summary>
+        /// Withdraws a stated amount into a target account from the specified pot.
+        /// </summary>
+        /// <param name="destinationAccountId">Account to deposit into.</param>
+        /// <param name="potId">Pot to withdraw from.</param>
+        /// <param name="amount">Amount to withdraw.</param>
+        /// <returns>If the withdrawal was successful or not.</returns>
+        Task<bool> WithdrawFromPotAsync(string destinationAccountId, string potId, long amount);
     }
 }
